ad7bc7aae2dc59794497fd102b06d4291496a971
geras <gerasymenkoph@gmail.com>
2021-11-26 00:37:25 +0200

    Update rest_api.php
    frash update

--- rest_api.php
+++ rest_api.php
@@ -1,84 +1,63 @@
 <?php
 
-class ControllerExtensionFeedRestApi extends Controller {
+class ControllerExtensionFeedRestApi extends Controller
+{
+    /**
+     * Show product in json format.
+     */
+    public function products(): void
+    {
+        $this->load->model('catalog/product');
+        $result      = ['success' => true, 'products' => []];
+        $category_id = $this->request->get['category'] ?? 0;
+        $limit = $this->config->get('theme_'.$this->config->get('config_theme').'_product_limit');
 
-	private $debugIt = false;
-	
-	/*
-	* Get products
-	*/
-	public function products() {
+        if (isset($this->request->get['limit'])) {
+            $limit = (int)$this->request->get['limit'];
+        }
 
+        $products = $this->model_catalog_product->getProducts(
+            [
+                'filter_category_id' => $category_id,
+                'limit'              => $limit,
+            ]
+        );
+        
+        foreach ($products as $product) {
+            $image   = false;
+            $special = false;
+            
+            if ($product['image']) {
+                $image = '/image/'.$product['image'];
+            }
 
-		$this->load->model('catalog/product');
-	
-		$json = array('success' => true, 'products' => array());
-
-		/*check category id parameter*/
-		if (isset($this->request->get['category'])) {
-			$category_id = $this->request->get['category'];
-		} else {
-			$category_id = 0;
-		}
-
-			if (isset($this->request->get['limit'])) {
-			$limit = (int)$this->request->get['limit'];
-		} else {
-			$limit = $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit');
-		}
-
-
-		$products = $this->model_catalog_product->getProducts(array(
-			'filter_category_id'        => $category_id,
-		    'limit' => $limit,
-		));
-
-		foreach ($products as $product) {
-
-			if ($product['image']) {
-				$image = '/image/'.$product['image'];
-			} else {
-				$image = false;
-			}
-
-			if ((float)$product['special']) {
-				$special = $this->currency->format($this->tax->calculate($product['special'], $product['tax_class_id'], $this->config->get('config_tax')));
-			} else {
-				$special = false;
-			}
-
-			$json['products'][] = array(
-					'id'			=> $product['product_id'],
-					'name'			=> $product['name'],
-					'description'	=> $product['description'],
-					'pirce'			=> $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'))),
-					'href'			=> $this->url->link('product/product', 'product_id=' . $product['product_id']),
-					'thumb'			=> $image,
-					'special'		=> $special,
-					'rating'		=> $product['rating']
-			);
-		}
-
- 		$this->response->addHeader('Content-Type: application/json');
-
-		if ($this->debugIt) {
-			echo '<pre>';
-			print_r($json);
-			echo '</pre>';
-		} else {
-			$this->response->setOutput(json_encode($json));
-		}
-	}
-
-	public function check() {
-
-		//in future i add safe condition to products()
-		if($this->debugIt)
-		print_r($_SERVER);
-
-
-	}
-
-	
-
+            if ((float)$product['special']) {
+                $special = $this->currency->format(
+                    $this->tax->calculate(
+                        $product['special'],
+                        $product['tax_class_id'],
+                        $this->config->get('config_tax')
+                    )
+                );
+            }
+            
+            $price = $this->currency->format(
+                $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'))
+            );
+            
+            $result['products'][] = [
+                'id'          => $product['product_id'],
+                'name'        => $product['name'],
+                'description' => $product['description'],
+                'price'       => $price,
+                'href'        => $this->url->link('product/product', 'product_id='.$product['product_id']),
+                'thumb'       => $image,
+                'special'     => $special,
+                'rating'      => $product['rating'],
+            ];
+        }
+        
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($result));
+    }
 }
